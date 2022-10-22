dc3167c125a736da45f777e97c77219c74bf3720
cool-man-vk <rrvigneshkumar2002@gmail.com>
2022-10-22 14:02:44 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,33 @@
+import classes from './styles/Navbar.module.css';
+
+const Navbar = () => {
+    const navOptions = [
+        {name:'Home',link:'/'},
+        {name:'Get started',link:'/get-started'}
+    ];
+
+    return(
+        <div className={classes.navbar}>
+            <div className={classes.logo}>
+                <a href='/'><h3>Vignesh</h3></a>
+            </div>
+            <div className={classes.options}>
+                <ul className={classes.menus}>
+                    {
+                        navOptions.map(
+                            (opt) => {
+                                return (
+                                    <a href={opt.link}>
+                                        <li>{opt.name}</li>
+                                    </a>
+                                );
+                            }
+                        )
+                    }
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar
